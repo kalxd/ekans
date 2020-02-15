6cf1d9b62e93854e6b1fd23043ff73c8baacde0d
荀徒之 <kalxd@126.com>
2020-02-15 13:52:35 +0800

    添加必要参数。

--- base/action.rkt
+++ base/action.rkt
@@ -11,10 +11,10 @@
   (发生错误 "尚未为此网站实现该行为！"))
 
 (define-generics Site
-  (->搜索 Site)
-  (->下载歌词 Site)
-  (->下载音乐 Site)
+  (->搜索 Site 查询)
+  (->下载歌词 Site 歌曲)
+  (->下载音乐 Site 歌曲)
 
-  #:fallbacks [(define (->搜索 self) (尚未实现))
-               (define (->下载歌词 self) (尚未实现))
-               (define (->下载音乐 self) (尚未实现))])
+  #:fallbacks [(define (->搜索 self 查询) (尚未实现))
+               (define (->下载歌词 self 歌曲) (尚未实现))
+               (define (->下载音乐 self 歌曲) (尚未实现))])

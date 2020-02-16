824dda67d1006bd841d8bbb51108d6e3f2c45796
荀徒之 <kalxd@126.com>
2020-02-16 14:22:47 +0800

    手册。

--- info.rkt
+++ info.rkt
@@ -1,8 +1,15 @@
 #lang info
-(define collection "ekans")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/lyric-searcher.scrbl" ())))
+
+(define name "Ekans")
+(define gracket-launcher-libraries '("main.rkt"))
+(define gracket-launcher-names '("ekans"))
+
+(define deps '("base"
+               "gui-lib"))
+(define build-deps '("scribble-lib"
+                     "racket-doc"
+                     "rackunit-lib"))
+(define scribblings '(("scribblings/main.scrbl" ())))
 (define pkg-desc "歌词白嫖器")
 (define version "0.1.0")
 (define pkg-authors '(XG.Ley))
---
+++ scribblings/main.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[ekans
+                    racket/base]]
+
+@title{Ekans}
+@author{XG.Ley}
+
+歌词白嫖下载器
+
+待续……
